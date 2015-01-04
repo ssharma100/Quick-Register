4aff3e4a4450c74306028d96f33c8601aa71d5e4
ssharma100 <solect123>
2015-01-04 00:00:03 -0800

    Moving resources config to UrlMapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -9,5 +9,9 @@ class UrlMappings {
 
         "/"(view:"/index")
         "500"(view:'/error')
+
+        // Mappings For REST Controllers
+        "/categories"(resources:"category") {
+        }
 	}
 }
--- grails-app/domain/real/estate/Cstegory.groovy
+++ grails-app/domain/real/estate/Cstegory.groovy
@@ -1,8 +1,5 @@
 package real.estate
 
-import grails.rest.*
-
-@Resource(uri='/category')
 class Cstegory {
     String name
     CondoList condo
